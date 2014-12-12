cdc9f96b30723710e15117400e1f5c5b2b3194f6
Alexander Willner <alexander.willner@tu-berlin.de>
2014-12-12 11:50:10 +0100

    added automated acceptance tests

---
+++ acceptance-test/run.sh
@@ -0,0 +1,7 @@
+java \
+  -jar automated-testing-4.1.12-SNAPSHOT.jar \
+  -c be.iminds.ilabt.jfed.lowlevel.api.test.TestAggregateManager3 \
+  --authorities-file conf/cli.authorities \
+  -p conf/cli.mbp.properties
+
+open $(ls -t|head -n1)/result.html
