9a7fdf812adfb650f25b56052f4247cd2fd6d86a
Alexander Willner <alexander.willner@tu-berlin.de>
2014-12-12 15:42:41 +0100

    cleanup

--- acceptance-test/run.sh
+++ acceptance-test/run.sh
@@ -2,6 +2,6 @@ java \
   -jar automated-testing-4.1.12-SNAPSHOT.jar \
   -c be.iminds.ilabt.jfed.lowlevel.api.test.TestAggregateManager3 \
   --authorities-file conf/cli.authorities \
-  -p conf/cli.mbp.properties
+  -p conf/cli.localhost.properties
 
 open $(ls -t|head -n1)/result.html
