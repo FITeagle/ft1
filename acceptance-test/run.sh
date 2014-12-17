b72dfc62776e581f19713a3540aa3f4ac727a7a7
dnehls <daniel.nehls@tu-berlin.de>
2014-12-17 15:38:12 +0100

    added fixed rspec

--- acceptance-test/run.sh
+++ acceptance-test/run.sh
@@ -1,5 +1,5 @@
 java \
-  -jar automated-testing-4.1.12-SNAPSHOT.jar \
+  -jar automated-testing.jar \
   -c be.iminds.ilabt.jfed.lowlevel.api.test.TestAggregateManager3 \
   --authorities-file conf/cli.authorities \
   -p conf/cli.localhost.properties
