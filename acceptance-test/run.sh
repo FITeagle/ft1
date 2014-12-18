28302ff3f667c903871f5ed8b91d1b56c61d073a
Alexander Willner <alexander.willner@tu-berlin.de>
2014-12-18 20:28:38 +0100

    cleanup

--- acceptance-test/run.sh
+++ acceptance-test/run.sh
@@ -1,5 +1,15 @@
+#!/usr/bin/env bash
+
+_VERSION="5.4-dev/r2339"
+_URL="http://jfed.iminds.be/releases/${_VERSION}/jar/jfed_cli.tar.gz"
+_PATH="jfed_cli"
+
+if [ ! -d "${_PATH}" ]; then 
+  curl "${_URL}" | tar -zx
+fi
+
 java \
-  -jar automated-testing.jar \
+  -jar "${_PATH}/automated-testing.jar" \
   -c be.iminds.ilabt.jfed.lowlevel.api.test.TestAggregateManager3 \
   --authorities-file conf/cli.authorities \
   -p conf/cli.localhost.properties
