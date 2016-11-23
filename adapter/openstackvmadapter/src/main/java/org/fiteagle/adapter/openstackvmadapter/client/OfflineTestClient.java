a48a7bc035242116ceba0bcfe070cd857c241b6a
aor <alexander.ortlieb@fokus.fraunhofer.de>
2016-11-23 14:27:38 +0100

    Added comments with the StackOverflow Licenses and Links to the thread

--- adapter/openstackvmadapter/src/main/java/org/fiteagle/adapter/openstackvmadapter/client/OfflineTestClient.java
+++ adapter/openstackvmadapter/src/main/java/org/fiteagle/adapter/openstackvmadapter/client/OfflineTestClient.java
@@ -247,6 +247,14 @@ public class OfflineTestClient extends OpenstackClient {
 		return convertStreamToString(in);
 	}
 
+	  /*
+	  Parts (or the complete Code) of the following method(s) were inspired or copied
+	  from StackOverFlow/StackExchange.
+	  Because of the Licensing of StackOverFlow under the CC-BY-SA [*¹] and MIT[*²] License this comment
+	  has to link the original code:
+	  
+	  Link: https://stackoverflow.com/a/5445161
+	  */
 	private String convertStreamToString(InputStream is) {
 		@SuppressWarnings("resource")
 		Scanner s = new Scanner(is).useDelimiter("\\A");
@@ -254,3 +262,25 @@ public class OfflineTestClient extends OpenstackClient {
 	}
 
 }
+
+/*
+[1] https://creativecommons.org/licenses/by-sa/3.0/
+
+
+[2] https://opensource.org/licenses/MIT
+	The MIT License (MIT)
+	Copyright (c) 2016 StackExchange
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files 
+	(the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, 
+	publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
+	subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+	MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+	FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+	WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
